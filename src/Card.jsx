54e982fb05ff7d522d9cafc8bc2fb2c40cc3a560
Josh Shavers <shaversj@gmail.com>
2023-08-18 15:03:34 -0500

    Mobile and Desktop Changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ title, content }) => {
     <div className={"mx-6 bg-white py-[.9rem] font-KumbhSans text-[13.25px] font-thin text-very-dark-grayish-blue lg:mx-0 lg:text-[14px]"}>
       <button className={"flex w-full items-center justify-center"} onClick={handleOpenClick}>
         <h2 className={`${isOpen ? "font-bold" : ""}`}>{title}</h2>
-        <img className={`${isOpen ? "ml-auto rotate-180 lg:mr-4" : "ml-auto lg:mr-4"}`} src={arrow} alt={""} />
+        <img className={`${isOpen ? "ml-auto rotate-180 lg:mr-4" : "ml-auto lg:mr-4"}`} src={arrow} alt={"Expand Section"} />
       </button>
       {isOpen && <div className={"pr-7 pt-2 text-[12px] text-dark-grayish-blue"}>{content}</div>}
     </div>
