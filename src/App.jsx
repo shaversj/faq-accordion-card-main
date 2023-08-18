075a2ab5337684e4d18ec006909b7b078fc19c5f
Josh Shavers <shaversj@gmail.com>
2023-08-18 15:02:09 -0500

    Mobile and Desktop Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FaqLayout from "./FaqLayout.jsx"
 function App() {
   return (
     <>
-      <div className={"flex min-h-screen items-center justify-center bg-gradient-to-b from-soft-violet to-soft-blue"}>
+      <div className={"flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-soft-violet to-soft-blue"}>
         <FaqLayout />
       </div>
     </>
